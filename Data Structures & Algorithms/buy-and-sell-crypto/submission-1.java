496ed3441c5c53dc3e9bf9ba447f39c73897bd53
Sai Mounik <96139149+SaiMounik07@users.noreply.github.com>
2026-05-13 22:32:09 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int ma = 0;
+        for(int i = 0;i<prices.length-1;i++){
+            if(prices[i+1] - prices[i] >=0){
+                ma = Math.max(prices[i+1] - prices[i],ma);
+                prices[i+1] = prices[i];
+            }
+        }
+        return ma;
+    }
+}
